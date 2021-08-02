c116923340019c33dcd50ce4ae9c19bd8f4aedd3
Kaan Koç <kock@mef.edu.tr>
2021-08-03 00:08:11 +0300

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Kaan Koç on 3.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
